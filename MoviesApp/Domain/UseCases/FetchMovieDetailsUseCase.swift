9311c9b04bf07b772cd402032f1e922d91e1a115
Mohamed Abd-Elhamed Ahmed <mohamedabdelhamed382@gmail.com>
2025-12-31 00:51:02 +0200

    Add FetchMovieDetailsUseCase
    - Implements execute(movieId:) using async/await
    - Fetches movie details from MoviesRepository
    - Returns MovieDetails domain model
    - Handles APIError and database errors, mapped to AppError
    - Prepares use case for Movie Details screen integration

--- MoviesApp/Domain/UseCases/FetchMovieDetailsUseCase.swift
+++ MoviesApp/Domain/UseCases/FetchMovieDetailsUseCase.swift
@@ -5,3 +5,17 @@
 //  Created by Mohamed abdelhamed on 30/12/2025.
 //
 
+import Combine
+
+final class FetchMovieDetailsUseCase {
+
+    private let repository: MoviesRepository
+
+    init(repository: MoviesRepository) {
+        self.repository = repository
+    }
+
+    func execute(movieId: Int) async throws -> MovieDetails {
+            return try await repository.fetchMovieDetails(id: movieId)
+    }
+}

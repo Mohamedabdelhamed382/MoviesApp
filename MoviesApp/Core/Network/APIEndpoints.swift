94cb2ef1485e5e38228b90e7105404f66e2858af
Mohamed Abd-Elhamed Ahmed <mohamedabdelhamed382@gmail.com>
2025-12-30 21:07:37 +0200

    Fix TMDb authorization header

--- MoviesApp/Core/Network/APIEndpoints.swift
+++ MoviesApp/Core/Network/APIEndpoints.swift
@@ -19,7 +19,11 @@ struct Endpoint {
     let path: String
     let method: HTTPMethod
     var queryItems: [URLQueryItem] = []
-    var headers: [String: String]? = nil
+    var headers: [String: String] = [
+        "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwOGYxNzAwYmM4ZWJkYTUzMDJlOTM4MDI4YTlkMDM4NyIsIm5iZiI6MTYzNzYxNDk2Ni43ODcwMDAyLCJzdWIiOiI2MTljMDU3NjQ5NzU2MDAwNjE1YjdjMzciLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.U_maexpx43Ga1dvPnLnQlQJDAs0VP9yX_1sYlkjHRgk",
+        "accept": "application/json"
+    ]
+
     var body: Data? = nil
 
     func url() -> URL? {
